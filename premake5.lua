1b4e0ae70431349e81030240eeacb5e01ed145ac
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-08-24 18:20:48 +0200

    glm support

--- premake5.lua
+++ premake5.lua
@@ -15,6 +15,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Harboe/vendor/GLFW/include"
 IncludeDir["Glad"] = "Harboe/vendor/GLAD/include"
 IncludeDir["ImGui"] = "Harboe/vendor/imgui"
+IncludeDir["glm"] = "Harboe/vendor/glm"
 
 include "Harboe/vendor/GLFW"
 include "Harboe/vendor/Glad"
@@ -35,7 +36,9 @@ project "Harboe"
     files
     {
         "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/vendor/glm/glm/**.hpp",
+        "%{prj.name}/vendor/glm/glm/**.inl"
     }
 
     includedirs
@@ -44,7 +47,8 @@ project "Harboe"
         "%{prj.name}/vendor/spdlog/include",
         "%{IncludeDir.GLFW}",
         "%{IncludeDir.Glad}",
-        "%{IncludeDir.ImGui}"
+        "%{IncludeDir.ImGui}",
+        "%{IncludeDir.glm}"
     }
 
     links
@@ -104,7 +108,8 @@ project "Sandbox"
     includedirs
     {
         "Harboe/vendor/spdlog/include",
-        "Harboe/src"
+        "Harboe/src",
+        "%{IncludeDir.glm}"
     }
 
     links
