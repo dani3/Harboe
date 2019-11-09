9759018bfc17445aa0951bc188b476e709f86c48
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-11-09 11:42:12 +0100

    2D Renderer transforms

--- Harboe/src/Platform/OpenGL/OpenGLShader.h
+++ Harboe/src/Platform/OpenGL/OpenGLShader.h
@@ -16,6 +16,10 @@ namespace Harboe
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
 
+		virtual void SetFloat3(const std::string& name, const glm::vec3& value) override;
+		virtual void SetFloat4(const std::string& name, const glm::vec4& value) override;
+		virtual void SetMat4(const std::string& name, const glm::mat4& value) override;
+
 		virtual const std::string& GetName() const override;
 
 		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -31,7 +31,8 @@ void Sandbox2D::OnUpdate(Harboe::Timestep ts)
 	Harboe::RenderCommand::Clear();
 
 	Harboe::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Harboe::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Harboe::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Harboe::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 1.0f }, { 0.2f, 0.2f, 0.3f, 1.0f });
 	Harboe::Renderer2D::EndScene();
 }
 
