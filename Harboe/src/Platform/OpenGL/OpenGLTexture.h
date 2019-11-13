06667cd173e9ac7c1b865e16a2cc8499069bd742
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-11-13 17:44:15 +0100

    Shader unified into one

--- Harboe/src/Platform/OpenGL/OpenGLTexture.h
+++ Harboe/src/Platform/OpenGL/OpenGLTexture.h
@@ -2,16 +2,21 @@
 
 #include "Harboe/Renderer/Texture.h"
 
+#include <glad/glad.h>
+
 namespace Harboe
 {
 	class OpenGLTexture2D : public Texture2D
 	{
 	public:
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
 
 		virtual uint32_t GetWidth() const override { return m_Width; }
 		virtual uint32_t GetHeight() const override { return m_Height; }
+
+		virtual void SetData(void* data, uint32_t size) override;
 
 		virtual void Bind(uint32_t slot) const override;
 
@@ -22,5 +27,7 @@ namespace Harboe
 		uint32_t m_Height;
 
 		uint32_t m_RendererID;
+
+		GLenum m_InternalFormat, m_DataFormat;
 	};
 }
